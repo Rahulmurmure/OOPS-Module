fc917e9ada6e09d8ae9d75a4d2d27ba1a93fad6c
Rahulmurmure <murmurerahul@gmail.com>
2025-03-11 16:26:42 +0530

    Add files via upload

---
+++ Assignment1/Demoq2.java
@@ -0,0 +1,13 @@
+class Demoq2{
+	int num = 20;
+	double d1 = num;
+	void display(){
+		System.out.println(num);
+		System.out.println(d1);
+		}
+	public static void main(String[] args){ 
+	Demoq2 obj1 = new Demoq2();
+	obj1.display();
+
+	}
+}
